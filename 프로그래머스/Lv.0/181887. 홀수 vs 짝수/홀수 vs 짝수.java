376f43ca6d4746da66e3800a97eb93f2586372e9
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-01-13 17:08:38 +0900

    [level Lv.0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/181887. 홀수 vs 짝수/홀수 vs 짝수.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        int odd =0;
+        int even =0;
+        
+        for(int i=0;i<num_list.length;i++){
+            if(i%2==0){ // 홀수번째 원소
+                odd += num_list[i];
+            }
+            else if(i%2==1){
+                even += num_list[i];
+            }
+        }
+        
+        if(odd>even){
+            answer = odd;
+        }
+        else if(even>odd){
+            answer = even;
+        }
+        else if(even==odd){
+            answer = odd;
+        }
+        
+        return answer;
+    }
+}
