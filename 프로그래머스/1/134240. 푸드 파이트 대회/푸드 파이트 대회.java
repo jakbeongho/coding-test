ce7c1779e3e8af4a5761f5de8fcf57b72054a081
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-01-21 23:25:15 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 11.20 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -1,19 +1,13 @@
 class Solution {
     public String solution(int[] food) {
-        String answer = "";
-        
-        StringBuilder halfAnswer = new StringBuilder();
-        
-        for(int i=1;i<food.length;i++){
-            for(int j=0;j<food[i]/2;j++){
-                halfAnswer.append(i);
+        String answer = "0";
+
+        for (int i = food.length - 1; i > 0; i--) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer = i + answer + i; 
             }
         }
-        
-        answer = halfAnswer.toString() + "0" + halfAnswer.reverse().toString();
-        
-       
-        
+
         return answer;
     }
 }
