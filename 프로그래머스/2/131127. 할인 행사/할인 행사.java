8fb87cd3fd2ef70113a30994810c3a2e207a2ba7
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-01-23 23:51:11 +0900

    [level 2] Title: 할인 행사, Time: 0.08 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,28 +1,28 @@
 import java.util.*;
 
 class Solution {
-    public int solution(String[] want, int[] number, String[] discount) {
-        int answer = 0;
-        ArrayList<String> list = new ArrayList<>();
+        public int solution(String[] want, int[] number, String[] discount) {
+            int answer = 0;
+            int left = 0;
+            int right = 9;
+            Map<String, Integer> map = new HashMap<>();
+            for(int i =0;i<10;i++) map.put(discount[i], map.getOrDefault(discount[i],0)+1);
+            while(right < discount.length){
+                if(check(map,want,number)) answer++;
 
-        for (int i=0; i<want.length; i++) {
-            while (number[i]-- > 0) {
-                list.add(want[i]);
+                right++;
+                if(right < discount.length)
+                    map.put(discount[right], map.getOrDefault(discount[right],0)+1);
+                map.put(discount[left],map.get(discount[left])-1);
+                left++;
             }
-        }
 
-        for (int i=0; i<discount.length-list.size()+1;i ++) {
-            ArrayList<String> l = (ArrayList<String>) list.clone();
-            for (int j=i; j<i+list.size(); j++) {
-                if (l.contains(discount[j])) {
-                    l.remove(discount[j]);
-                } else {
-                    break;
-                }
+            return answer;
+        }
+        private boolean check(Map<String,Integer> map,String[] want,int[] number){
+            for(int i =0;i<want.length;i++){
+                if(map.getOrDefault(want[i],0) != number[i]) return false;
             }
-            answer += l.size()==0 ? 1 : 0;
+            return true;
         }
-
-        return answer;
     }
-}
