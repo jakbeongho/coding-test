0bf77bc7061ff3a1a672fcb5330e596d13064140
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-01-23 23:53:03 +0900

    [level 2] Title: 할인 행사, Time: 0.05 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,28 +1,28 @@
 import java.util.*;
 
 class Solution {
-        public int solution(String[] want, int[] number, String[] discount) {
-            int answer = 0;
-            int left = 0;
-            int right = 9;
-            Map<String, Integer> map = new HashMap<>();
-            for(int i =0;i<10;i++) map.put(discount[i], map.getOrDefault(discount[i],0)+1);
-            while(right < discount.length){
-                if(check(map,want,number)) answer++;
+    public int solution(String[] want, int[] number, String[] discount) {
+        int answer = 0;
 
-                right++;
-                if(right < discount.length)
-                    map.put(discount[right], map.getOrDefault(discount[right],0)+1);
-                map.put(discount[left],map.get(discount[left])-1);
-                left++;
+        Map<String,Integer> wantList = new HashMap<>();
+        Map<String,Integer> discountList = new HashMap<>();
+
+        for(int i=0;i<want.length;i++){
+            wantList.put(want[i], number[i]);
+        }
+
+        for(int i=0;i<=discount.length-10;i++){
+            discountList.clear();
+
+            for(int j=i;j<i+10;j++){
+                 discountList.put(discount[j],discountList.getOrDefault(discount[j],0)+1);
             }
 
-            return answer;
-        }
-        private boolean check(Map<String,Integer> map,String[] want,int[] number){
-            for(int i =0;i<want.length;i++){
-                if(map.getOrDefault(want[i],0) != number[i]) return false;
+            if(wantList.equals(discountList)){
+                answer += 1;
             }
-            return true;
         }
+
+        return answer;
     }
+}
