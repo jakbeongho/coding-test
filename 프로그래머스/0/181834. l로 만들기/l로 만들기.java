2cfaa8bfc020fe61495226dfe7dae2094b318245
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-01-24 17:20:26 +0900

    [level 0] Title: l로 만들기, Time: 14.98 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,16 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        
-        char[] tmp = myString.toCharArray();
-        
-        for(int i=0;i<tmp.length;i++){
-            if(tmp[i]<'l'){
-                tmp[i] = 'l';
-            }
-        }
-        
-        String answer = new String(tmp);
-        
-        return answer;
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
