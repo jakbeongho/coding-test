06841899e68cbf462063ce815cfba5a400a3da07
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-01-30 22:01:51 +0900

    [level 1] Title: 추억 점수, Time: 0.03 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+import java.util.HashMap;
+
+
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        
+        Map<String,Integer> score = new HashMap<>();
+        
+        for(int i=0; i<name.length;i++){
+            score.put(name[i], yearning[i]);
+        }
+        
+        for(int i=0; i<photo.length;i++){
+            
+            for(int j=0; j<photo[i].length;j++){
+                if(score.get(photo[i][j]) != null){
+                    answer[i] += score.get(photo[i][j]);     
+                }
+            }
+            
+        }
+        
+        return answer;
+    }
+}
