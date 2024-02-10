5827c7bc5b1ede46bc72a16df65da891deeb96b4
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-02-10 17:31:40 +0900

    [level 1] Title: 과일 장수, Time: 0.50 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+        
+        Arrays.sort(score);
+        
+        for(int i=score.length-m;i>=0;i-=m){
+            answer += score[i]*m;
+        }
+        
+        if(score.length<m){
+            return 0;
+        }
+        
+        return answer;
+    }
+}
