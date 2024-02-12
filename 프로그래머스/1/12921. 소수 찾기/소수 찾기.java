c3cd2b39219bbb940fc8ad22a156e8690e80224d
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-02-12 23:50:44 +0900

    [level 1] Title: 소수 찾기, Time: 200.47 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(int i=2;i<=n;i++){
+            if(check(i)){
+                answer += 1;
+            }
+        }
+        
+        
+        return answer;
+    }
+    
+    public boolean check(int n){
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
