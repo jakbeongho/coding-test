c1cf5b01674153747c94150d18a4f19fb4812d3a
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-03-25 23:55:15 +0900

    [level 2] Title: 프로세스, Time: 0.51 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,34 +1,30 @@
-import java.util.PriorityQueue;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Collections;
 
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
         
-        PriorityQueue<Integer> rank = new PriorityQueue<>(Collections.reverseOrder());
+        List<Integer> rank = new ArrayList<>();
         
         for(int i=0;i<priorities.length;i++){
-            rank.offer(priorities[i]);
+            rank.add(priorities[i]);
         }
+        
+        Collections.sort(rank, Collections.reverseOrder());
         
         while(!rank.isEmpty()){
-            
-            for(int i=0;i<priorities.length;i++){
-                
-                if(rank.peek()==priorities[i]){
-                    rank.poll();
+            for(int i=0;i<priorities.length;i++){   
+                if(rank.get(0)==priorities[i]){
+                    rank.remove(0);
                     answer += 1;
-                    
                     if(i==location){
                         return answer;
-                    }
-                    
+                    }      
                 }
-                
             }
         }
-        
-        
         return answer;
     }
 }
