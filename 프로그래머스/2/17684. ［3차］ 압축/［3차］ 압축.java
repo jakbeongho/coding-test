8352f53ec5b4feed16964211d173815a80f0b812
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-03-30 19:18:34 +0900

    [level 2] Title: [3차] 압축, Time: 15.79 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,51 +1,31 @@
-import java.util.*;
+import java.util.ArrayList;
 
 class Solution {
-    public int[] solution(String msg) {
-        
-        Map<String,Integer> LZW = new HashMap<>();
-        
-        String[] AtoZ = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-        
-        for(int i=0;i<AtoZ.length;i++){
-            LZW.put(AtoZ[i],i+1);
-        }
-        
-        List<Integer> answerList = new ArrayList<>();
-        
-        int index = 27;
-        
-        StringBuilder sb = new StringBuilder();
-        
-        int wIndex = 0;
-            
-        while(true){
-        
-            sb.append(msg.charAt(wIndex));
-                
-            if(LZW.containsKey(sb.toString())){
-                wIndex+=1;
-                
-                if(wIndex>=msg.length()){
-                    answerList.add(LZW.get(sb.toString()));
-                    break;
-                }
-            }
-            else{
-                LZW.put(sb.toString(),index);
-                sb.setLength(sb.length()-1);
-                answerList.add(LZW.get(sb.toString()));
-                sb.setLength(0);
-                index +=1;
-            }
-        }
-        
-        int[] answer = new int[answerList.size()];
-        
-        for(int i=0;i<answerList.size();i++){
-            answer[i] = answerList.get(i);
-        }
-        
-        return answer;
+  public int[] solution(String msg) {
+    ArrayList<String> dic = new ArrayList<String>();
+    ArrayList<Integer> result = new ArrayList<Integer>();
+
+    for(int i = 0 ; i < 26; i++) {
+        dic.add(String.valueOf((char)('A'+i)));
     }
+
+    for(int i = 0 ; i < msg.length() ; i++) {
+        for(int j = dic.size()-1 ; j >= 0 ; j--) {
+            if(msg.substring(i).startsWith(dic.get(j))) {
+                i += dic.get(j).length()-1;
+                result.add(j+1);
+                if(i+1 < msg.length())
+                    dic.add(dic.get(j)+msg.charAt(i+1));
+                break;
+            }
+        }
+    }
+
+    int[] answer = new int[result.size()];
+
+    for(int i = 0 ; i < result.size() ; i++) 
+        answer[i] = result.get(i);
+
+    return answer;  
+  }
 }
