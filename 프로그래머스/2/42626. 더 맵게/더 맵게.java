4ef2af9924d1f74e7ee9d1acadf7ed4e6a79d4cf
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-03-31 16:22:59 +0900

    [level 2] Title: 더 맵게, Time: 1809.83 ms, Memory: 144 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        
+        for (int s : scoville) {
+            queue.add(s);
+        }
+        
+        while(queue.peek()<K){
+            
+            int first = queue.poll();
+            
+            if(queue.peek()==null){
+                return -1;
+            }
+            
+            int second = queue.poll();
+            
+            queue.add(first+second*2);
+            answer+=1;
+        
+        }
+        
+        return answer;
+    }
+}
