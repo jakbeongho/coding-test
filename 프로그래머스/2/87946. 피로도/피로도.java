20a73e0b572472bfc5642f046e7a80e0890d7c3b
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-03-31 17:43:27 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,28 +1,33 @@
 class Solution {
     
-    public int answer;
-    public boolean[] clear;
+    boolean[] clear;
+    int count =0;
     
     public int solution(int k, int[][] dungeons) {
+        int answer = -1;
         
         clear = new boolean[dungeons.length];
         
-        allfind(0, k, dungeons);
+        dfs(dungeons,0,k);
+        
+        answer = count;
         
         return answer;
     }
     
-    public void allfind(int count,int k,int[][] dungeons){
+    public void dfs(int[][] dungeons,int depth, int k){
         
         for(int i=0;i<dungeons.length;i++){
-            if(!clear[i] && k >= dungeons[i][0]){
+            
+            if(k>=dungeons[i][0] && !clear[i]){
                 clear[i] = true;
-                allfind(count+1,k - dungeons[i][1], dungeons);
+                dfs(dungeons,depth+1,k-dungeons[i][1]);
                 clear[i] = false;
             }
+            
         }
         
-        answer = Math.max(answer,count);
+        count = Math.max(count,depth);
+        
     }
-    
 }
