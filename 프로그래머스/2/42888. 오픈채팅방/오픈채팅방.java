b2860ec5ad7d86d4b05afdceef26c9face53ca15
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-04-09 21:55:10 +0900

    [level 2] Title: 오픈채팅방, Time: 127.73 ms, Memory: 164 MB -BaekjoonHub

---
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] record) {
+        String[] answer = {};
+        List<String> answerList = new ArrayList<>();
+
+        Map<String, String> map = new HashMap<>();
+
+        for(int i = 0; i<record.length; i++){
+            String[] strArr = record[i].split(" ");
+
+            if(strArr[0].equals("Enter")){
+                map.put(strArr[1],strArr[2]);
+            }
+            else if(strArr[0].equals("Change")){
+                map.put(strArr[1],strArr[2]);
+            }
+        }
+
+        for(int i =0; i<record.length; i++){
+            String[] strArr = record[i].split(" ");
+
+            if(strArr[0].equals("Enter")){
+                answerList.add(map.get(strArr[1])+"님이 들어왔습니다.");
+            }
+            else if(strArr[0].equals("Leave")){
+                answerList.add(map.get(strArr[1])+"님이 나갔습니다.");
+            }
+        }
+
+        answer = answerList.stream().toArray(String[]::new);
+        
+        return answer;
+    }
+}
