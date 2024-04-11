e9219864a4b9bba6917e13fc73e4f41d217a31a3
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-04-11 21:44:46 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.88 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,202 +1,79 @@
-import java.time.LocalTime;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        FeeTable feeTable = new FeeTable(fees[0], fees[1], fees[2], fees[3]);
 
-        Records record = Arrays.stream(records)
-                .map(Record::parse)
-                .collect(Collectors.collectingAndThen(
-                        Collectors.groupingBy(Record::getCar), Records::new));
+        Map<Integer,Integer> use = new HashMap<>();
+        Map<Integer,Integer> totalTime = new HashMap<>();
 
-        return record.calculateFees(feeTable).stream()
-                .mapToInt(i -> i)
-                .toArray();
-    }
+        int basicTime = fees[0];
+        int basicPrice = fees[1];
+        int unitTime = fees[2];
+        int unitPrice = fees[3];
 
-    private static class Car implements Comparable<Car> {
-        private final String number;
+        for(int i=0;i<records.length;i++){
 
-        public Car(String number) {
-            this.number = number;
-        }
+            String[] car = records[i].split(" ");
+            int useTime = 0;
+            int carNumber = Integer.parseInt(car[1]);
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Car car = (Car) o;
-            return Objects.equals(number, car.number);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(number);
-        }
-
-        @Override
-        public int compareTo(Car other) {
-            return this.number.compareTo(other.number);
-        }
-    }
-
-    private static class Records {
-        private static final LocalTime DEFAULT_OUT_TIME = LocalTime.parse("23:59");
-        private final Map<Car, List<Record>> records;
-
-        public Records(Map<Car, List<Record>> records) {
-            this.records = records;
-        }
-
-        public List<Integer> calculateFees(FeeTable feeTable) {
-            return records.entrySet().stream()
-                    .sorted(Map.Entry.comparingByKey())
-                    .map(Map.Entry::getValue)
-                    .map(record -> calculateFee(record, feeTable))
-                    .collect(Collectors.toList());
-        }
-
-        private Integer calculateFee(List<Record> record, FeeTable feeTable) {
-            Collections.sort(record);
-
-            int index = 0;
-            int differ = 0;
-
-            while (index < record.size()) {
-                differ += calculateDiffer(record, index);
-                index += 2;
+            if(car[2].equals("IN")){
+                use.put(carNumber,convertMinute(car[0]));
             }
-
-            if (differ <= feeTable.getBaseTimeMinutes()) {
-                return feeTable.getBaseFee();
+            else if(car[2].equals("OUT")){
+                useTime = convertMinute(car[0]) - use.get(carNumber);
+                use.remove(carNumber);
+                totalTime.put(carNumber,totalTime.getOrDefault(carNumber,0)+useTime);
             }
-
-            int withoutBase = differ - feeTable.getBaseTimeMinutes();
-            int remainder = withoutBase % feeTable.getUnitTimeMinutes() > 0 ? 1 : 0;
-
-            return feeTable.getBaseFee() + (remainder + withoutBase / feeTable.getUnitTimeMinutes()) * feeTable.unitFee;
         }
 
-        private Integer calculateDiffer(List<Record> record, int index) {
-            Record in = record.get(index);
-            Record out = getNextRecord(record, in.getCar(), index);
+        Set<Integer> noExit = new HashSet<>(use.keySet());
 
-            return in.getDifferFrom(out);
+        for(int carNumber : noExit) {
+            int useTime = convertMinute("23:59") - use.get(carNumber);
+            use.remove(carNumber);
+            totalTime.put(carNumber,totalTime.getOrDefault(carNumber,0)+useTime);
         }
 
-        private Record getNextRecord(List<Record> record, Car car, int index) {
-            if (index + 1 >= record.size()) {
-                return new Record(RecordType.OUT, car, DEFAULT_OUT_TIME);
-            }
+        int[] answer = new int[totalTime.size()];
 
-            return record.get(index + 1);
+        TreeSet<Integer> sortCarNumber = new TreeSet<>(totalTime.keySet());
+
+        int index = 0;
+        for(Integer carNumber : sortCarNumber){
+            answer[index] = convertMoney(totalTime.get(carNumber),basicTime,basicPrice,unitTime,unitPrice);
+            index++;
         }
+
+        return answer;
     }
 
-    private static class Record implements Comparable<Record> {
-        private final RecordType type;
-        private final Car car;
-        private final LocalTime time;
+    public int convertMinute(String time){
 
-        public Record(RecordType type, Car car, LocalTime time) {
-            this.type = type;
-            this.car = car;
-            this.time = time;
-        }
+        int result = 0;
 
-        public static Record parse(String rawRecord) {
-            String[] record = rawRecord.split(" ");
-            LocalTime time = LocalTime.parse(record[0]);
-            Car car = new Car(record[1]);
-            RecordType recordType = RecordType.findType(record[2]);
+        String[] timeArray = time.split(":");
 
-            return new Record(recordType, car, time);
-        }
+        result = Integer.parseInt(timeArray[0])*60 + Integer.parseInt(timeArray[1]);
 
-        public Car getCar() {
-            return car;
-        }
-
-        public boolean isOut() {
-            return type.isOut();
-        }
-
-        public boolean isInBaseTime(Record other, int baseTime) {
-            return getDifferFrom(other) <= baseTime;
-        }
-
-        public Integer getDifferFrom(Record other) {
-            int here = TimeUtils.toMinutes(time);
-            int otherTime = TimeUtils.toMinutes(other.time);
-            return Integer.max(here, otherTime) - Integer.min(here, otherTime);
-        }
-
-        @Override
-        public int compareTo(Record o) {
-            return this.time.compareTo(o.time);
-        }
+        return result;
     }
 
-    private static class TimeUtils {
-        public static Integer toMinutes(LocalTime time) {
-            return time.getHour() * 60 + time.getMinute();
+    public int convertMoney(int time,int basicTime, int basicPrice, int unitTime, int unitPrice){
+
+        int result = 0;
+
+        if(time<=basicTime){
+            return basicPrice;
         }
+        else{
+            time -= basicTime;
+            result += basicPrice;
+            int count = (int) Math.ceil(((double)time/unitTime));
+            result += count*unitPrice;
+        }
+
+        return result;
     }
 
-    private enum RecordType {
-        IN,
-        OUT;
-
-        public static RecordType findType(String type) {
-            return Arrays.stream(values())
-                    .filter(t -> t.name().equals(type))
-                    .findAny()
-                    .orElseThrow(IllegalArgumentException::new);
-        }
-
-        public boolean isIn() {
-            return IN.equals(this);
-        }
-
-        public boolean isOut() {
-            return OUT.equals(this);
-        }
-    }
-
-    private static class FeeTable {
-        private final Integer baseTimeMinutes;
-        private final Integer baseFee;
-        private final Integer unitTimeMinutes;
-        private final Integer unitFee;
-
-        public FeeTable(Integer baseTimeMinutes, Integer baseFee,
-                        Integer unitTimeMinutes, Integer unitFee) {
-            this.baseTimeMinutes = baseTimeMinutes;
-            this.baseFee = baseFee;
-            this.unitTimeMinutes = unitTimeMinutes;
-            this.unitFee = unitFee;
-        }
-
-        public Integer getBaseTimeMinutes() {
-            return baseTimeMinutes;
-        }
-
-        public Integer getBaseFee() {
-            return baseFee;
-        }
-
-        public Integer getUnitTimeMinutes() {
-            return unitTimeMinutes;
-        }
-
-        public Integer getUnitFee() {
-            return unitFee;
-        }
-    }
 }
