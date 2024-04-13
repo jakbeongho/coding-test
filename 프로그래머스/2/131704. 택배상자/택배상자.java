c2c793c9351ea1edd7220a8a36190a8aa6b89747
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-04-13 23:41:18 +0900

    [level 2] Title: 택배상자, Time: 25.68 ms, Memory: 124 MB -BaekjoonHub

---
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        
+        Stack<Integer> auxiliaryBelt = new Stack<>();
+        auxiliaryBelt.push(0);
+        
+        int index=0;
+        
+        for(int i=0;i<order.length;i++){
+        
+            if(order[index]==i+1){
+                index+=1;
+                continue;
+            }
+           
+            if(order[index]==auxiliaryBelt.peek()){
+                index+=1;
+                i-=1;
+                auxiliaryBelt.pop();
+                continue;
+            }
+            
+            auxiliaryBelt.push(i+1);
+        }
+        
+        for(int i=index;i<order.length;i++){
+            
+            if(order[i]==auxiliaryBelt.peek()){
+                index+=1;
+                auxiliaryBelt.pop();
+            }
+            else{
+                break;
+            }
+            
+        }
+        
+        answer = index;
+        
+        return answer;
+    }
+}
