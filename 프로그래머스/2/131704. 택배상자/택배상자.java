77ba5abeda7462614b942cfed9b2d0964157f9e6
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-04-13 23:45:46 +0900

    [level 2] Title: 택배상자, Time: 26.40 ms, Memory: 142 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -1,18 +1,46 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] order) {
-        int idx = 0;
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < order.length; i++){
-            stack.push(i+1);
-            while(!stack.isEmpty()){
-                if (stack.peek() == order[idx]){
-                    stack.pop();
-                    idx++;
-                } else break;
+        int answer = 0;
+        
+        Stack<Integer> auxiliaryBelt = new Stack<>();
+        auxiliaryBelt.push(0);
+        
+        int index=0;
+        
+        for(int i=0;i<order.length;i++){
+        
+            if(order[index]==i+1){
+                index+=1;
+                continue;
             }
+           
+            if(order[index]==auxiliaryBelt.peek()){
+                index+=1;
+                i-=1;
+                auxiliaryBelt.pop();
+                continue;
+            }
+            
+            auxiliaryBelt.push(i+1);
+            
         }
-
-        return idx;
+        
+        for(int i=index;i<order.length;i++){
+            
+            if(order[i]==auxiliaryBelt.peek()){
+                index+=1;
+                auxiliaryBelt.pop();
+            }
+            else{
+                break;
+            }
+            
+        }
+        
+        answer = index;
+        
+        return answer;
     }
 }
