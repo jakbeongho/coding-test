d1f5fa19dfb9d9a4425c1103b995ab50297aef9a
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-04-16 23:05:20 +0900

    [level 2] Title: 숫자 변환하기, Time: 1.61 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,23 +1,34 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 class Solution {
-    
-   public int solution(int x, int y, int n) {
-    int[] dp = new int[y + 1];
-    for (int i = x; i < y + 1; i++) {
-        if (i != x && dp[i] == 0) {
-            dp[i] = -1;
-            continue;
+
+    private static final int MAX_VALUE = 1_000_000;
+    private final int[] visitCount = new int[MAX_VALUE + 1];
+    private final Queue<Integer> queue = new ArrayDeque<>();
+
+    public int solution(int x, int y, int n) {
+        return bfs(x, y, n);
+    }
+
+    private int bfs(int source, int target, int n) {
+        queue.add(source);
+        while (!queue.isEmpty()) {
+            int value = queue.poll();
+            if (value == target) {
+                return visitCount[value];
+            }
+            addQueue(target, value, value + n);
+            addQueue(target, value, value * 2);
+            addQueue(target, value, value * 3);
         }
-        if (i * 2 <= y) {
-            dp[i * 2] = (dp[i * 2] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 2]);
-        }
-        if (i * 3 <= y) {
-            dp[i * 3] = (dp[i * 3] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i * 3]);
-        }
-        if (i + n <= y) {
-            dp[i + n] = (dp[i + n] == 0) ? dp[i] + 1 : Math.min(dp[i] + 1, dp[i + n]);
+        return -1;
+    }
+
+    private void addQueue(int target, int value, int nextValue) {
+        if (nextValue <= target && visitCount[nextValue] == 0) {
+            queue.add(nextValue);
+            visitCount[nextValue] = visitCount[value] + 1;
         }
     }
-    return dp[y];
-    }
-    
 }
