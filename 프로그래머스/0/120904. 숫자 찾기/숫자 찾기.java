e621586686f204f5262d831e36558097076be16e
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-05-02 21:43:24 +0900

    [level 0] Title: 숫자 찾기, Time: 0.02 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = 0;
+        
+        String temp = Integer.toString(num);
+        
+        char c = (char) (48+k);
+        
+        if(temp.indexOf(c)==-1){
+            return -1;
+        }
+        
+        answer = temp.indexOf(c)+1;
+        
+        return answer;
+    }
+}
