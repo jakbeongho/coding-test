c249d3824917113af7d9949b689ade92febde036
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-05-15 23:59:30 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 72.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+        int answer = 0;
+        
+        for(int i =0; i<my_string.length(); i++){
+            if(my_string.substring(i).equals(is_suffix)){    
+                answer = 1;
+            }
+        
+        }
+        
+        return answer;
+    }
+}
