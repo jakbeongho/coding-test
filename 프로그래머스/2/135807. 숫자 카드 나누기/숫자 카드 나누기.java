89ab23a96931c4ee01db629a7fe6e454fd1b96cb
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-05-29 11:14:01 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 1.41 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -1,98 +1,52 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public int solution(int[] arrayA, int[] arrayB) {
         int answer = 0;
-        
-        Arrays.sort(arrayA);
-        Arrays.sort(arrayB);
-    
-        List<Integer> divisorsA = getDivisors(arrayA[0]);
-        List<Integer> divisorsB = getDivisors(arrayB[0]);
-        
-        List<Integer> divisorsArrayA = getArrayDivisors(divisorsA, arrayA);
-        List<Integer> divisorsArrayB = getArrayDivisors(divisorsB, arrayB);
-        
-        int a = noDivisor(divisorsArrayA, arrayB);
-        int b = noDivisor(divisorsArrayB, arrayA);
-        
-        answer = Math.max(a,b);
-        
-        if(answer==1){
-            answer =0;
-        }
-        
+
+        int aMin = getMin(arrayA);
+        int aGcd = getArraysGcd(arrayA, aMin);
+
+        int bMin = getMin(arrayB);
+        int bGcd = getArraysGcd(arrayB, bMin);
+
+        answer = getAnswer(aGcd, arrayB, answer);
+        answer = getAnswer(bGcd, arrayA, answer);
+
         return answer;
     }
-    
-    public List<Integer> getDivisors(int a){
-        
-        List<Integer> divisors = new ArrayList<>();
-        
-        for(int i=1;i<=Math.sqrt(a);i++){
-            if(a%i==0){
-                divisors.add(i);
-                divisors.add(a/i);
-            }  
-        }
-        
-        return divisors;
-        
-    }
-    
-    public List<Integer> getArrayDivisors(List<Integer> divisors, int[] array){
-        
-        List<Integer> ArrayDivisors = new ArrayList<>();
-        
-        boolean check = true;
-        
-        for(int i=0;i<divisors.size();i++){
-            int divisor = divisors.get(i); 
-            check = true;
-            
-            for(int j=0;j<array.length;j++){
-                if(array[j]%divisor!=0){
-                    check = false;
-                    break;
-                }
-            }
-            
-            if(check){
-                ArrayDivisors.add(divisor);
-            }
-            
-        }
-        
-        Collections.sort(ArrayDivisors, Collections.reverseOrder());
-        
-        return ArrayDivisors;
-        
-    }
-    
-    public int noDivisor(List<Integer> divisors, int[] array){
-        
-        int result = 0;
-        boolean check = true;
-        
-        for(int i=0;i<divisors.size();i++){
-            int divisor = divisors.get(i); 
 
-            check = true;
-            
-            for(int j=0;j<array.length;j++){
-                if(array[j]%divisor==0){
-                    check = false;
-                    break;
-                }
-            }
-            
-            if(check){
-                result = divisor;
-                break;
+    private static int getAnswer(int gcd, int[] array, int answer) {
+        for (int num : array) {
+            if (num % gcd == 0) {
+                return Math.max(answer, 0);
             }
         }
-        
-        return result;
-    } 
-    
+        return Math.max(answer, gcd);
+    }
+
+    private static int getMin(int[] array) {
+        return Arrays.stream(array)
+                .reduce(Math::min)
+                .getAsInt();
+    }
+
+    private static int getArraysGcd(int[] array, int min) {
+        int gcd = 1;
+        for (int i = 2; i <= min; i++) {
+            if (isGcd(array, i)) {
+                gcd = i;
+            }
+        }
+        return gcd;
+    }
+
+    private static boolean isGcd(int[] array, int i) {
+        for (int num : array) {
+            if (num % i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
