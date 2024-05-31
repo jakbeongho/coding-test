3f3837f57003c3f59564e3d04c24ea204141e153
jakbeongho <143082875+jakbeongho@users.noreply.github.com>
2024-05-31 13:20:25 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.42 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        
+        Arrays.sort(numbers);
+        
+        answer = numbers[0]*numbers[1];
+        
+        if(numbers[0]*numbers[1]<numbers[numbers.length-2]*numbers[numbers.length-1]){
+            answer= numbers[numbers.length-2]*numbers[numbers.length-1];
+        }
+        
+        return answer;
+    }
+}
